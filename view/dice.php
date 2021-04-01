3052e3a6195bc3d2557e559005d6676b4775e053
Rilr20 <nrrl99@gmail.com>
2021-04-01 11:05:11 +0200

    Graphical dice added to view

--- view/dice.php
+++ view/dice.php
@@ -10,34 +10,68 @@ use Rilr\Dice\Game;
 
 $header = $header ?? null;
 $message = $message ?? null;
+
 // var_dump($_SESSION);
 // var_dump($_SESSION["running"]);
+
 ?>
 <h1><?= $header ?></h1>
 <p>Roll the die and get as close to 21 but not over it!</p>
-
 <?php
 
 if ($_SESSION["running"] == "true") { ?>
     <!--När denna är set så ska gameAction raderas och en ny knapp reset ska visas-->
-    <p>Computer: <?= $computerSum ?></p>
-    <p>Player: <?= $playerRoll ?></p>
+    <!-- när datorn kör så ska det visas många tärningar, när man kastar igen så ska tärningar visas  -->
+    <!-- Spara förra täringarna för $computerDice -->
+    <h3>Computer: <?= $computerSum ?></h3>
+    <div class="dice-box">
+        <?php
+        // echo $computerDice;
+        foreach ($computerDice as $die) {
+            echo $die;
+        }
+        ?>
+    </div>
+    <h3>Player: <?= $playerRoll ?></h3>
+    <div class="dice-box">
+        <?php
+        foreach ($playerDice as $die) {
+            echo $die;
+        }
+        ?>
+    </div>
     <form action="" method="post">
-        <input type="submit" name="gameAction" value="roll" />
+        <button class="game-button" type="submit" name="gameAction" value="roll" />Roll</button>
         <input type="hidden" name="computer" value="<?= $computerSum ?>">
         <input type="hidden" name="player" value="<?= $playerRoll ?>">
-        <input type="submit" name="gameAction" value="stay" />
+        <?php $_SESSION["computerDice"] = serialize($computerDice); ?>
+        <button class="game-button" type="submit" name="gameAction" value="stay" />Stay</button>
     </form>
-    <?php
-} else if ($_SESSION["running"] == "intermission") {
-    ?>
+<?php
+} else if ($_SESSION["running"] == "intermission") { //When you've stayed
+?>
 
     <h2><?= $gameText ?></h2>
-    <p>Computer: <?= $computerSum ?></p>
-    <p>Player: <?= $playerRoll ?></p>
+    <h3>Computer: <?= $computerSum ?></h3>
+    <div class="dice-box">
+        <?php
+        foreach ($computerDice as $die) {
+            echo $die;
+        }
+        ?>
+    </div>
+    <h3>Player: <?= $playerRoll ?></h3>
+    <div class="dice-box">
+        <?php
+        foreach ($playerDice as $die) {
+            echo $die;
+        }
+        ?>
+    </div>
+    <div class="dice-box"></div>
     <form action="" method="post">
-        <button name="gameAction" , type="submit" value="reset">Reset</button>
-        <button name="gameAction" , type="submit" value="resetscore">Reset Score</button>
+        <button class="game-button" name="gameAction" , type="submit" value="reset">Reset</button>
+        <button class="game-button" name="gameAction" , type="submit" value="resetscore">Reset Score</button>
     </form>
     <?php
     if (isset($gameText)) {
@@ -51,14 +85,14 @@ if ($_SESSION["running"] == "true") { ?>
 
     ?>
 
-    <?php
-} else { //när progarmmet är nytt ?>
+<?php
+} else { //When program starts
+?>
     <p>Play with 1 or 2 dice?</p>
     <form action="" method="post">
-        <button name="options" type="submit" value="1">1 die</button>
-
-        <button name="options" type="submit" value="2">2 Dice</button>
+        <button class="game-button" name="options" type="submit" value="1">1 die</button>
+        <button class="game-button" name="options" type="submit" value="2">2 Dice</button>
     </form>
-    <?php
+<?php
 }
 ?>
