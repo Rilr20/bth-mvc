601d94d2deeb8bcd5f584f5d4e14681698e0caa8
Rilr20 <nrrl99@gmail.com>
2021-04-02 10:20:18 +0200

    changes to dice.php

--- src/Dice/Dice.php
+++ src/Dice/Dice.php
@@ -9,17 +9,31 @@ namespace Rilr\Dice;
  */
 class Dice
 {
+    /**
+     * @var int $sides Number of sides on the Dice
+     * @var int $sides Saves the last throw value of the dice
+     */
     public int $sides;
     private int $latestThrow = 0;
+    /**
+     * Get funktion for $sides
+     */
     public function __construct($sides)
     {
         $this->sides = $sides;
     }
+    /**
+     * Gets a random number depending on dice sides
+     * @return int the amoun the dice random was 
+     */
     public function throw(): int
     {
         $this->latestThrow = rand(1, $this->sides);
         return $this->latestThrow;
     }
+    /**
+     * @return int returns the last thrown value of the dice
+     */
     public function getLastRoll(): int
     {
         return $this->latestThrow;
