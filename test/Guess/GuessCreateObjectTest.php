2758939db9e61e1429d3346d5d68c02f04bb0559
Rilr20 <nrrl99@gmail.com>
2021-04-13 21:53:50 +0200

    Unit test for guess class

--- test/Guess/GuessCreateObjectTest.php
+++ test/Guess/GuessCreateObjectTest.php
@@ -60,4 +60,42 @@ class GuessCreateObjectTest extends TestCase
         $exp = 42;
         $this->assertEquals($exp, $res);
     }
-}
+
+    public function testMakeGuess() 
+    {
+        $guess = new Guess(20, 4);
+        $this->assertInstanceOf("\Mos\Guess\Guess", $guess);
+
+        $res = $guess->makeGuess(21);
+        $exp = "to high...";
+        $this->assertEquals($exp, $res);
+
+        $res = $guess->makeGuess(19);
+        $exp = "to low...";
+        $this->assertEquals($exp, $res);
+
+        $res = $guess->makeGuess(20);
+        $exp = "correct!!!";
+        $this->assertEquals($exp, $res);
+
+    }
+    public function testNoMoreGuesses()
+    {
+        $guess = new Guess(20, 0);
+        $this->assertInstanceOf("\Mos\Guess\Guess", $guess);
+
+        $res = $guess->makeGuess(33);
+        $exp = "no guesses left.";
+        $this->assertEquals($exp, $res);
+    }
+    public function testOutOfBounds()
+    {
+        $guess = new Guess(20);
+        $this->assertInstanceOf("\Mos\Guess\Guess", $guess);
+
+        $this->expectException(GuessException::class);
+        $res = $guess->makeGuess(-1);
+        // $exp = "no guesses left.";
+        // $this->assertEquals($exp, $res);
+    }
+} 
